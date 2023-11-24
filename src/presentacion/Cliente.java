ffc521ee09b8eaa281553e05dc926facd5b8a766
Per1cosele <144569263+Per1cosele@users.noreply.github.com>
2023-11-23 21:54:03 -0500

    UpdatePresentationPackages

--- src/presentacion/Cliente.java
+++ src/presentacion/Cliente.java
@@ -6,8 +6,19 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
+/**
+ * Clase que representa la aplicación principal
+ * @author Soky
+ */
 public class Cliente {
+    
+    /**
+     * Constructo de la clase Cliente.
+     * Crea y muestra una ventana centrada al iniciar la aplicaión.
+     */
+    
     public Cliente() {
+        //Creacion de una nueva ventana y ajuste de su posición en la pantalla.
         JFrame frame = new Ventana();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Dimension frameSize = frame.getSize();
@@ -22,12 +33,20 @@ public class Cliente {
         frame.setVisible(true);
     }
 
+    /**
+     * Metodo principal de la aplicacíón.
+     * Configura la apariencia y comportamiento del sistema y crea una instancia cliente.   
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         try {
+            //Configuración de la apariencia y comportamiento del sistema
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
         }
+        //Creacion de una instancia cliente que permita iniciar el sismtema
         new Cliente();
     }
 }
