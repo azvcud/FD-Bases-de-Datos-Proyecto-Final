2d054f5ea9c802038e4c4562962b72c7f4e2a557
azvcud <134235962+azvcud@users.noreply.github.com>
2023-11-26 13:58:26 -0500

    Detalles
    Listo para que Julián trabaje

--- src/aplicacion/InicioSesion.java
+++ src/aplicacion/InicioSesion.java
@@ -4,6 +4,8 @@
  */
 package aplicacion;
 
+import gestor.MensajeroGestor;
+import gestor.SolicitanteGestor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import presentacion.VistaInicioSesion;
@@ -16,12 +18,17 @@ public class InicioSesion implements ActionListener {
     
     private Aplicacion mediador;
     private VistaInicioSesion vista;
+    
     //Gestor
+    private MensajeroGestor gestorMensajero;
+    private SolicitanteGestor gestorSolicitante;
     
     //Pueden modificar el constructor en caso de que necesite uno o más gestores
-    public InicioSesion(VistaInicioSesion vista, Aplicacion mediador) {
+    public InicioSesion(VistaInicioSesion vista, SolicitanteGestor gestorSolicitante, MensajeroGestor gestorMensajero, Aplicacion mediador) {
         this.vista = vista;
         this.mediador = mediador;
+        this.gestorSolicitante = gestorSolicitante;
+        this.gestorMensajero = gestorMensajero;
         
         this.vista.btnInicioMensajero.addActionListener(this);
         this.vista.btnInicioSolicitante.addActionListener(this);
@@ -33,6 +40,8 @@ public class InicioSesion implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == vista.btnRegistroMensajero) { mediador.notificar(this, "Registrar mensajero"); }
         if (e.getSource() == vista.btnRegistroSolicitante) { mediador.notificar(this, "Registrar solicitante"); }
+        if (e.getSource() == vista.btnInicioMensajero) { /*JOption 1*/ } 
+        if (e.getSource() == vista.btnInicioSolicitante) { /*JOption 2*/ } 
     }
 
     public void desplegar(boolean estado) {
